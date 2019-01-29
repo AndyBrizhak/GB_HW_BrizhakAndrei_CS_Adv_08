75b5791ec55fe796e945df6439c3ad837f0476b6
AndyBrizhak <andrewbrizhak@gmail.com>
2019-01-29 15:49:28 +0800

    + класс DepController в веб приложении, который описывает Департаменты

---
+++ WebAppEmpDep/Controllers/DepController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebAppEmpDep.Models;
+
+namespace WebAppEmpDep.Controllers
+{
+    public class DepController : ApiController
+    {
+        private List<Department> _deps = new List<Department>()
+        {
+            new Department {DepId = 1, DepName = "Приемная"},
+            new Department {DepId = 2, DepName = "Прачечная"},
+            new Department {DepId = 3, DepName = "Морг"}
+        };
+
+        public List<Department> GetAllDeps()
+        {
+            return _deps;
+        }
+
+        public IHttpActionResult GetDep(int id)
+        {
+            var dep = _deps.FirstOrDefault((p) => p.DepId == id);
+            if (dep == null)
+            {
+                return NotFound();
+            }
+            return Ok(dep);
+        }
+
+
+
+
+    }
+}
