185c64893764f7e1d70ac81a90e256ba326058a8
AndyBrizhak <andrewbrizhak@gmail.com>
2019-01-29 09:31:13 +0800

    +класс работников в веб приложении и назначили свойства

---
+++ WebAppEmpDep/Models/Employee.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebAppEmpDep.Models
+{
+    public class Employee
+    {
+        public string FName { get; set;}
+        public string LName { get; set; }
+        public string Age { get; set; }
+        public int DepID { get; set; }
+    }
+}
