49791744f57fdb1f118e2d749f67253e128d8c5c
AndyBrizhak <andrewbrizhak@gmail.com>
2019-01-29 09:35:10 +0800

    +класс департаментов в веб приложении и его свойства

---
+++ WebAppEmpDep/Models/Department.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebAppEmpDep.Models
+{
+    public class Department
+    {
+        public int DepId { get; set; }
+        public string DepName { get; set; }
+    }
+}
